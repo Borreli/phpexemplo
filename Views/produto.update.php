6f15c64ac98423231f3522b7fcc910e84ec58622
AnnaHermenegildo <54596765+AnnaHermenegildo@users.noreply.github.com>
2020-07-06 19:24:43 -0300

    produto edit

---
+++ Views/produto.update.php
@@ -0,0 +1,19 @@
+<?php
+header('location: pessoa.index.php'); // redireciona para o local indicado
+
+spl_autoload_register(function ($class_name) {
+    include '..\\'.$class_name . '.php';
+});
+
+use Models\Produto;
+use Db\Persiste;
+
+if ( isset($_POST['id']) && isset($_POST['nome_produto']) && isset($_POST['data_vencimento_produto]) && isset($_POST['valor_produto]))
+{	
+	$persiste = new Persiste();
+	// id foi colocado 0 pois será gerado automaticamente pelo banco de dados
+	$p = new Produto($_POST['id'],$_POST['nome_produto'],$_POST['data_vencimento_produto']$_POST['valor_produto']);
+	$persiste->UpdateProduto($p);
+}
+
+?>
